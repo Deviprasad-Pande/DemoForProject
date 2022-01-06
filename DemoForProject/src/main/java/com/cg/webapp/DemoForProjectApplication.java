590ebf29a2ac809453e2d2a0b58bedac1d45f697
DP <deviprasadp2004@gmail.com>
2022-01-06 12:10:38 +0530

    Updated on 12:12pm

---
+++ DemoForProject/src/main/java/com/cg/webapp/DemoForProjectApplication.java
@@ -0,0 +1,13 @@
+package com.cg.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoForProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoForProjectApplication.class, args);
+	}
+
+}
